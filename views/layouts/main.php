d63cb974d22cca43bf1990876484e009ee178fd3
luchik21 <luchezavr640595@gmail.com>
2021-12-24 16:31:35 +0200

    first commit

---
+++ views/layouts/main.php
@@ -0,0 +1,177 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\assets\PublicAsset;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\helpers\Url;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+PublicAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>" class="h-100">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body class="d-flex flex-column h-100">
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<?php $this->beginBody() ?>
+
+<nav class="navbar main-menu navbar-default">
+    <div class="container">
+        <div class="menu-content">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/"><img src="/public/images/logopet.jpg" alt=""></a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                <ul class="nav navbar-nav text-uppercase">
+                    <li><a data-toggle="dropdown" class="dropdown-toggle" href="/">Home</a>
+                    </li>
+                </ul>
+                <div class="i_con">
+                    <ul class="nav navbar-nav text-uppercase">
+                    <?php if(Yii::$app->user->isGuest):?>
+                            <li><a href="<?= Url::toRoute(['auth/login'])?>">Login</a></li>
+                            <li><a href="<?= Url::toRoute(['auth/signup'])?>">Register</a></li>
+                        <?php else: ?>
+                            <?= Html::beginForm(['/auth/logout'], 'post')
+                            . Html::submitButton(
+                                'Logout (' . Yii::$app->user->identity->name . ')',
+                                ['class' => 'btn btn-link logout', 'style'=>"padding-top:10px;"]
+                            )
+                            . Html::endForm() ?>
+                        <?php endif;?>
+                    </ul>
+                </div>
+
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+
+
+<?= $content?>
+
+
+<footer class="footer-widget-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <aside class="footer-widget">
+                    <div class="about-img"><img src="/public/images/logopet.jpg" alt=""></div>
+                    <div class="about-content">Do you love pets? Do you have PETS!?
+                                            This is the place where pets lovers gather to meow, gaff, purr, and roar about, what else? Cats! Dogs! And others!
+                    </div>
+                    <div class="address">
+                        <h4 class="text-uppercase">contact Info</h4>
+                        <p>mypets.com</p>
+                    </div>
+                </aside>
+            </div>
+
+            <div class="col-md-4">
+                <aside class="footer-widget">
+                    <h3 class="widget-title text-uppercase">Testimonials</h3>
+
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <!--Indicator-->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                        </ol>
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <div class="single-review">
+                                    <div class="review-text">
+                                        <p>This is very good site about pets! I post good news about our little friends everyday!</p>
+                                    </div>
+                                    <div class="author-id">
+                                        <img src="/public/images/authoroleg.jpg" alt="">
+
+                                        <div class="author-text">
+                                            <h4>Oleg</h4>
+
+                                            <h4>Admin, postcreator</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="single-review">
+                                    <div class="review-text">
+                                        <p>I like cats and dogs! So i want to share posts about pets!</p>
+                                    </div>
+                                    <div class="author-id">
+                                        <img src="/public/images/author.png" alt="">
+
+                                        <div class="author-text">
+                                            <h4>Sophia</h4>
+
+                                            <h4>Redactor</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </aside>
+            </div>
+            <div class="col-md-4">
+                <aside class="footer-widget">
+                    <h3 class="widget-title text-uppercase">Donate</h3>
+
+
+                    <div class="custom-post">
+                        <div>
+                            <a href="#"><img src="/public/images/donate.jpg" alt=""></a>
+                        </div>
+                        <div>
+                            <a href="#" class="text-uppercase">Donations</a>
+                            <span class="p-date">you can donate if you can</span>
+                        </div>
+                    </div>
+                </aside>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copy">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="text-center">&copy; 2021 <a href="#">We </a><i
+                            class="fa fa-heart"></i> our <a href="/">PETS</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ views/site/single.php
@@ -0,0 +1,50 @@
+<?php
+use yii\helpers\Url;
+?>
+
+<!--main content start-->
+<div class="main-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <article class="post">
+                    <div class="post-thumb">
+                        <a href="blog.html"><img src="<?= $article->getImage();?>" alt=""></a>
+                    </div>
+                    <div class="post-content">
+                        <header class="entry-header text-center text-uppercase">
+                            <h6><a href="<?= Url::toRoute(['site/category','id'=>$article->category->id])?>"> <?= $article->category->title?></a></h6>
+
+                            <h1 class="entry-title"><a href="<?= Url::toRoute(['site/view','id'=>$article->id])?>"><?= $article->title?></a></h1>
+
+                        </header>
+                        <div class="entry-content">
+                            <?= $article->content?>
+                        </div>
+                       
+                        <div class="social-share">
+							<span
+                                class="social-share-title pull-left text-capitalize">By <?= $article->autor->name?> On <?= $article->getDate();?></span>
+                            <ul class="text-center pull-right">
+                                <li><a class="s-facebook" href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a></li>
+                                <li><a class="s-twitter" href="https://twitter.com/"><i class="fa fa-twitter"></i></a></li>
+                                <li><a class="s-instagram" href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </article>
+
+             <?= $this->render('/partials/comment', [
+                 'article'=>$article,
+                 'comments'=>$comments,
+                 'commentForm'=>$commentForm
+             ])?>
+            </div>
+            <?= $this->render('/partials/sidebar', [
+                'popular'=>$popular,
+                'recent'=>$recent,
+                'categories'=>$categories
+            ]);?>
+        </div>
+    </div>
+</div>
